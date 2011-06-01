abca1cc83cbd9545854088ba99d54bf2781838ba
Sergio Díaz <sergiooreamhyn@gmail.com>
2011-05-31 19:24:09 -0600

    I actually started coding. It's still pretty raw but, I'll eventually get there.

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
+<?php
+    include('routes.php');  
+?>
 <pre>
-<?php var_dump($_SERVER['REQUEST_URI']); ?>
-<br>
-<?php var_dump(explode('/', $_SERVER['REQUEST_URI'])); ?>
+<?php 
+
+	var_dump($_SERVER['REQUEST_URI']);  
+    	
+	$settings = array(
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.71 Safari/534.24'
+	);
+	
+    Rout::get('/', function($params){
+        var_dump($params);
+    }, $settings);
+
+?>
--- routes.php
+++ routes.php
@@ -31,14 +31,45 @@
 
 /**
 * The basic Routs class that'll end up doing all the work.
-*
-* @package Routs
 */
 class Rout
 {
     
-    function __construct()
+    /**
+     * This function handles GET requests for the specified route.
+     *
+	 * @param string $route The route to be handled by this function.
+	 * @param callback $callback The function to be executed when the 
+	 * @param array $settings An array containing additional configurations.
+	 * 
+     * @return void
+     **/
+    public static function get($route, $callback, $settings = null) 
     {
-        # code...
-    }
+        if($_SERVER['REQUEST_METHOD'] != 'GET') return false; // Ignore request if it's not a get call.
+				
+		$params = self::getParams();
+		
+		$callback($params);
+   	}
+	
+	/**
+	* This function removes the directory data from the url and gets the parameters.
+	*
+	* @return array returns an array with all the parameters.
+	*/
+	private static function getParams() 
+	{
+		$params = explode('/', $_SERVER['REQUEST_URI']); // Break up the URL to find the parameters.
+		
+		if(dirname(__FILE__) != '/') { // If the file isn't in the root folder remove directories.
+			$script_deepness = count(explode('/', $_SERVER['SCRIPT_NAME'])); // Get the total number of folders the script is in.
+			for($i = 0; $i < $script_deepness - 1; $i++) {
+				array_shift($params); // Remove the folders from the params.
+			}
+		}
+		
+		return $params;		
+	}
+
 }
